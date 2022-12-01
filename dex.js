0a62463258bff9db67e1dc568dafb833e8351b70
Tolunay <tlnymtlu@gmail.com>
2022-12-01 22:23:32 +0300

    pokemon limit

--- dex.js
+++ dex.js
@@ -2,23 +2,24 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const PORT = process.env.PORT || 3200;
-
+const pokemonLimit = 905;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.listen(PORT, () =>{
-    console.log(`Server up on ${PORT}`)
-});
+
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/',(req, res) => {
-    res.setHeader("Content-Type", "text/html")
+    res.setHeader("Content-Type", "text/html");
     let id = Number(req.body.pokemonID);
-    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
+    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    if(id > pokemonLimit){
+        res.write(`There is only ${pokemonLimit} pokemon we have.And... I think that's the limit.Sorry`);
+    }
     https.get(url ,(response) => {
         let responseData = "";
         response.on('data',(chunk) =>{
@@ -29,20 +30,14 @@ app.post('/',(req, res) => {
             let pokemonInfo = JSON.parse(responseData);
             let pokemonName = pokemonInfo.name;
             let pokemonImage = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`
-            console.log(pokemonImage);
-
             res.write(`<img src=${pokemonImage}>`)
             res.send();
-        })
-    })
-})
+        });
+    });
+});
 
 
-
-
-
-
-
-
-
+app.listen(PORT, () =>{
+    console.log(`Server up on ${PORT}`)
+});
 
