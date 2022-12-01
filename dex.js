21fc3b42729d189098c5fd70450a86d4edc2266b
Tolunay <tlnymtlu@gmail.com>
2022-12-01 22:07:27 +0300

    getting pic

--- dex.js
+++ dex.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const app = express();
 const https = require('https');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3200;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.listen(PORT, () =>{
+    console.log(`Server up on ${PORT}`)
+});
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/',(req, res) => {
+    res.setHeader("Content-Type", "text/html")
     let id = Number(req.body.pokemonID);
     let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 
+    https.get(url ,(response) => {
+        let responseData = "";
+        response.on('data',(chunk) =>{
+            responseData += chunk;
+        });
+
+        response.on('end', () =>{
+            let pokemonInfo = JSON.parse(responseData);
+            let pokemonName = pokemonInfo.name;
+            let pokemonImage = `https://img.pokemondb.net/artwork/large/${pokemonName}.jpg`
+            console.log(pokemonImage);
+
+            res.write("<img src=" +pokemonImage + ">")
+            res.send();
+        })
+    })
 })
 
 
@@ -21,9 +42,7 @@ app.post('/',(req, res) => {
 
 
 
-app.listen(PORT, () =>{
-    console.log(`Server up on ${PORT}`)
-});
+
 
 
 
